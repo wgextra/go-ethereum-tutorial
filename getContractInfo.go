688f3edd8bb325c59900085a481fb98cf0ab1a0d
Takumi Kumagai <kumagai.takumi1223@gmail.com>
2018-03-29 14:53:04 +0900

    Extract contract information from the Blockchain.

---
+++ getContractInfo.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "context"
+    "log"
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/ethclient"
+    "io/ioutil"
+    "encoding/json"
+)
+
+type Config struct{
+    InfuraId    string `json:"infuraId"`
+    ContractHash    string `json:"contractHash"`
+}
+
+func main() {
+    bytes,err := ioutil.ReadFile("config.json")
+    if err != nil{
+        log.Fatalf("File Error!", err)
+    }
+
+    var config Config
+    if err := json.Unmarshal(bytes,&config);err != nil{
+        log.Fatalf("Json Error!", err)
+    }
+    conn, err := ethclient.Dial(fmt.Sprintf("https://mainnet.infura.io/%s",config.InfuraId))
+    if err != nil {
+        log.Fatalf("Dial Error!", err)
+    }
+    ctx := context.Background()
+    tx,pending,err := conn.TransactionByHash(ctx,common.HexToHash(config.ContractHash))
+    if(!pending){
+        fmt.Println(tx)
+    }
+}
+
